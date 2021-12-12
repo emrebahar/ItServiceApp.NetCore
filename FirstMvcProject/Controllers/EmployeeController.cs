732cb0500c7e735ceddb266c353458b97f10e281
Yunus Emre Bahar <eb471652@gmail.com>
2021-12-13 01:15:52 +0300

    Employee Create/Delete İmplemented

---
+++ FirstMvcProject/Controllers/EmployeeController.cs
@@ -0,0 +1,79 @@
+﻿using FirstMvcProject.Models;
+using FirstMvcProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMvcProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly NorthwindContext _northwindContext;
+        public EmployeeController(NorthwindContext northwindContext)
+        {
+            _northwindContext = northwindContext;
+        }
+        public IActionResult Index()
+        {
+            var data = _northwindContext.Employees.OrderBy(x => x.EmployeeId).ToList();
+            return View(data);
+        }
+        public IActionResult Detail(int id)
+        {
+            var employee = _northwindContext.Employees
+                .Include(x => x.Orders)
+                .FirstOrDefault(x => x.EmployeeId == id);
+            return View(employee);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(EmployeeViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            Employee employee = new Employee
+            {
+                FirstName = model.FirsName,
+                LastName = model.LastName,
+                Title = model.Title,
+                City = model.City,
+                Address = model.Adress,
+                BirthDate = model.BirthDate
+            };
+            _northwindContext.Employees.Add(employee);
+            try
+            {
+                _northwindContext.SaveChanges();
+                return RedirectToAction("Detail", new { id = employee.EmployeeId });
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError(string.Empty, $"{model.FirsName + model.LastName} eklenirken bir hata oluştu Tekrar deneyiniz.");
+                return View(model);
+            }
+        }
+        public IActionResult Delete(int id)
+        {
+            var deleteEmploye = _northwindContext.Employees.FirstOrDefault(x => x.EmployeeId == id);
+            try
+            {
+                _northwindContext.Employees.Remove(deleteEmploye);
+                _northwindContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Detail", new { id = id });
+            }
+            TempData["Silinen Çalışan"] = deleteEmploye.FirstName + " " + deleteEmploye.LastName;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FirstMvcProject/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMvcProject.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int EmployeeId { get; set; }
+        [Required(ErrorMessage = "Ad Alanı Gereklidir."), StringLength(10, ErrorMessage = "Ad Alanı En Fazla 15 Karakter olabilir!")]
+        [Display(Name = "Çalışan Ad")]
+        public string FirsName { get; set; }
+        [Required(ErrorMessage = "Soyad Alanı Gereklidir."), StringLength(20, ErrorMessage = "Soy ad Alanı En Fazla 15 Karakter olabilir!")]
+        [Display(Name = "Çalışan Soyad")]
+        public string LastName { get; set; }
+        [Display(Name = "Ünvan")]
+        public string Title { get; set; }
+        [Display(Name = "Şehir")]
+        public string City { get; set; }
+        [Display(Name = "Doğum Günü")]
+        public DateTime? BirthDate { get; set; }
+        [Display(Name = "Adres")]
+        public string Adress { get; set; }
+
+    }
+}
