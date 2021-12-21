c5edcc20d61152dc0c23bf291103413668a00ffa
Yunus Emre Bahar <eb471652@gmail.com>
2021-12-21 19:12:24 +0300

    LayoutUsage

---
+++ LayoutUsage/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LayoutUsage.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ LayoutUsage/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿console.log("MVC uygulaması çalıştı");
+
+
+var App = {
+    name: "MVC uygulaması",
+    selamla: function () {
+        return "Merhaba " + this.name;
+    }
+}
